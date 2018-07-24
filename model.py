13752a8ed1cf521eed632ae469fbf1d6512b308f
vagrant <vagrant@vagrant.vm>
2018-07-24 18:25:39 +0000

    parsed csv file into a postings database with model.py and seed.py

--- model.py
+++ model.py
@@ -1,7 +1,7 @@
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-
-db =SQLAlchemy()
+db = SQLAlchemy()
 
 
 class Posting(db.Model):
@@ -9,8 +9,9 @@ class Posting(db.Model):
 
     __tablename__ = "postings"
 
-    posting_id = db.Column(db.Integer, primary_key=True, autincrement=True)
+    posting_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     company = db.Column(db.String(64), nullable=True)
+    title = db.Column(db.String, nullable=False)
     description = db.Column(db.String, nullable=True)
     requirements = db.Column(db.String, nullable=True)
     qualifications = db.Column(db.String, nullable=False)
@@ -18,10 +19,26 @@ class Posting(db.Model):
     def __repr__(self):
         """shows posting"""
 
-        return f"""<Posting 
+        return f"""<Posting
                     posting_id={self.posting_id}
                     company={self.company}
+                    title={self.title}
                     description={self.description}
                     requirements={self.requirements}
                     qualifications={self.requirements}"""
 
+
+def connect_to_db(app):
+    """Connect to database."""
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///postings'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    db.app = app
+    db.init_app(app)
+
+app = Flask(__name__)
+
+if __name__ == "__main__":
+    connect_to_db(app)
+
+    db.create_all()
--- seed.py
+++ seed.py
@@ -1,1 +1,30 @@
 """Utility file to seed postings database from posting csv file"""
+
+from sqlalchemy import func
+from model import Posting
+from model import connect_to_db, db
+from model import app
+import csv
+
+
+def load_postings():
+    """load postings from job posting database."""
+
+    with open('data job posts.csv') as csvfile:
+        reader = csv.reader(csvfile, delimiter=",")
+        for row in reader:
+            title = row[2]
+            description, requirements, qualifications = row[11:14]
+
+            posting = Posting(title=title,
+                              description=description,
+                              requirements=requirements,
+                              qualifications=qualifications)
+
+            db.session.add(posting)
+
+
+    db.session.commit()
+
+connect_to_db(app)
+load_postings()

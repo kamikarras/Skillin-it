f17c124b10970e57565953e269f1b6c2909808a6
vagrant <vagrant@vagrant.vm>
2018-08-01 20:01:32 +0000

    refactored code that displays skills per job now that the data is all stored on a table

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 from flask import (Flask, render_template, redirect, request, flash, session)
 from flask_sqlalchemy import SQLAlchemy
-from model import Job, app, Posting, load_jobs, connect_to_db
+from model import Job, app, Posting, load_jobs, connect_to_db, Skill
 from jinja2 import StrictUndefined
 
 
@@ -18,12 +18,6 @@ def show_home():
 
     return render_template("homepage.html")
 
-@app.route('/skill_search', methods=["GET"])
-def get_skill():
-    """displays skill seach form"""
-
-    return render_template("skill_search.html")
-
 
 @app.route('/all_jobs')
 def job_list():
@@ -32,6 +26,13 @@ def job_list():
     jobs = Job.query.all()
 
     return render_template("all_jobs.html", jobs=jobs)
+
+
+@app.route('/skill_search', methods=["GET"])
+def get_skill():
+    """displays skill seach form"""
+
+    return render_template("skill_search.html")
 
 
 @app.route('/skill_search', methods=["POST"])
@@ -57,34 +58,43 @@ def show_skills():
 
     job_title = request.form.get('title')
 
-    postings = Posting.query.filter(Posting.title == job_title).all()
-    all_words = []
-
-    for posting in postings:
-        words = posting.qualifications.lower().split()
-        all_words.extend(words)
-
-    word_counts = {}
-    with open('filler.txt') as filler:
-        del_words = set(filler.read())
-        for word in all_words:
-            word = word.strip("-()/\,.:;* 1234567890")
-            if word not in del_words:
-                if word in word_counts:
-                    word_counts[word] += 1
-                else:
-                    word_counts[word] = 1
-
+    job = Job.query.filter(Job.title == job_title).one()
+    skill_ids = job.skills
     skills = []
+    for skill_id in skill_ids:
+        skill_name = db.session.query(Skill.skill).filter(Skill.skill_id == skill_id.skill_id).all()
+        skill_count = skill_id.count
+        skills.append([skill_count, skill_name[0][0]])
 
-    for count in range(len(word_counts)):
-        skill_count = 0
-        for word in word_counts:
-            if word_counts[word] > skill_count:
-                skill_count = word_counts[word]
-                skill = word
-        skills.append(skill)
-        del word_counts[skill]
+    skills = sorted(skills, reverse=True)
+
+    # all_words = []
+
+    # for skills in job.skills:
+    #     words = skills.qualifications.lower().split()
+    #     all_words.extend(words)
+
+    # word_counts = {}
+    # with open('filler.txt') as filler:
+    #     del_words = set(filler.read())
+    #     for word in all_words:
+    #         word = word.strip("-()/\,.:;* 1234567890")
+    #         if word not in del_words:
+    #             if word in word_counts:
+    #                 word_counts[word] += 1
+    #             else:
+    #                 word_counts[word] = 1
+
+    # skills = []
+
+    # for count in range(len(word_counts)):
+    #     skill_count = 0
+    #     for word in word_counts:
+    #         if word_counts[word] > skill_count:
+    #             skill_count = word_counts[word]
+    #             skill = word
+    #     skills.append(skill)
+    #     del word_counts[skill]
 
     return render_template("job_skills.html",
                             skills=skills,

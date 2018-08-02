aaeac5127a6aa1918d05011be850b4109f38b87d
vagrant <vagrant@vagrant.vm>
2018-08-02 00:03:01 +0000

    created a page to search by skill that give you all the jobs asking for that skill. Currently working on adding multiple skills to narrow results

--- server.py
+++ server.py
@@ -39,9 +39,19 @@ def get_skill():
 def show_jobs():
     """displays skill seach form"""
 
-    skill = request.form.get('skill')
+    skill_search = request.form.get('skill_search')
+    skill_search2 = request.form.get('another_skill')
+    skill = Skill.query.filter(Skill.skill==skill_search).one()
+    job_ids = skill.jobs
+    jobs = []
 
-    return render_template("skill_search.html")
+    for job_id in job_ids:
+        job_title = db.session.query(Job.title).filter(Job.job_id == job_id.job_id).one()
+        jobs.append(job_title[0])
+
+    return render_template("skill_search.html",
+                            jobs=jobs,
+                            skill_search=skill_search)
 
 
 @app.route('/job_skills', methods=["GET"])
@@ -70,37 +80,11 @@ def show_skills():
 
     skills = sorted(skills, reverse=True)[:length]
 
-    # all_words = []
-
-    # for skills in job.skills:
-    #     words = skills.qualifications.lower().split()
-    #     all_words.extend(words)
-
-    # word_counts = {}
-    # with open('filler.txt') as filler:
-    #     del_words = set(filler.read())
-    #     for word in all_words:
-    #         word = word.strip("-()/\,.:;* 1234567890")
-    #         if word not in del_words:
-    #             if word in word_counts:
-    #                 word_counts[word] += 1
-    #             else:
-    #                 word_counts[word] = 1
-
-    # skills = []
-
-    # for count in range(len(word_counts)):
-    #     skill_count = 0
-    #     for word in word_counts:
-    #         if word_counts[word] > skill_count:
-    #             skill_count = word_counts[word]
-    #             skill = word
-    #     skills.append(skill)
-    #     del word_counts[skill]
 
     return render_template("job_skills.html",
                             skills=skills,
-                            job_title=job_title)
+                            job_title=job_title,
+                            length=length)
 
 
 
